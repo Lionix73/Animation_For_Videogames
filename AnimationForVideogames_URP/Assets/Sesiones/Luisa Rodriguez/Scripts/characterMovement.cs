62439964ff4ef286941f090d860bcae62fffff44
LuisaRoech <luisa.rodrigueze@upb.edu.co>
2025-02-11 07:41:00 -0500

    Input controller y camara

--- AnimationForVideogames_URP/Assets/Sesiones/Luisa Rodriguez/Scripts/characterMovement.cs
+++ AnimationForVideogames_URP/Assets/Sesiones/Luisa Rodriguez/Scripts/characterMovement.cs
@@ -1,18 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 [RequireComponent(typeof(Animator))]
 
 public class CharacterMovement : MonoBehaviour
 {
-    [SerializeField] private float speedX;
-    [SerializeField] private float speedY;
+    [SerializeField] private Camera camera;
+    [SerializeField] private FloatDampener speedX;
+    [SerializeField] private FloatDampener speedY;
 
     private Animator animator;
 
     private int speedXHash;
     private int speedYHash;
+
+    public void OnMove(InputAction.CallbackContext ctx)
+    {
+        Vector2 InputValue = ctx.ReadValue<Vector2>();
+        speedX.targetValue = InputValue.x;
+        speedY.targetValue = InputValue.y;
+
+        Vector3 floorNormal = transform.up;
+        Vector3 cameraRealFoward = camera.transform.forward;
+        float angleInterpolator = Mathf.Abs(f:Vector3.Dot(lhs:cameraRealFoward, rhs:floorNormal));
+
+        Vector3 cameraFoward = Vector3.Lerp(cameraRealFoward, camera.transform.up, angleInterpolator).normalized;
+        Vector3 characterFoward = Vector3.ProjectOnPlane(vector: cameraFoward, floorNormal).normalized;
+        Debug.DrawLine(transform.position, transform.position + cameraFoward * 2, Color.magenta, duration: 5);
+
+    }
 
     private void Awake()
     {
@@ -23,9 +41,11 @@ public class CharacterMovement : MonoBehaviour
 
     private void Update()
     {
-        if (animator == null) return;
-    
-        animator.SetFloat(name:"SpeedX", speedX);
-        animator.SetFloat(name:"SpeedY", speedY);
+        speedX.Update();
+        speedY.Update();
+
+        animator.SetFloat(speedXHash, speedX.currentValue);
+        animator.SetFloat(speedYHash, speedY.currentValue);
+
     }
 }
